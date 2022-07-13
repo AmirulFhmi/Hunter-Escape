734c87d1507b13f5eaa6da56f187c19c23868351
AmirulFhmi <amirulfahmi.anim@gmail.com>
2022-07-13 23:08:28 +0800

    Commit game mechanics: Game manager script

--- Assets/Scripts/GameStartManager.cs
+++ Assets/Scripts/GameStartManager.cs
@@ -5,12 +5,39 @@ using Photon.Pun;
 
 public class GameStartManager : MonoBehaviour
 {
+
+    public static GameStartManager Instance;
+
+    [Header("Game Variable")]
+    [SerializeField] bool isStarting = false;
+    [SerializeField] bool isFinished = false;
     public GameObject mysteryBoxPrefab;
     public Transform[] spawnPoints;
+    public int playerCount = 0;
+
+    public enum GameState
+    {
+        Start,
+        End
+    }
+
+    public GameState gameState;
+
     // Start is called before the first frame update
     void Start()
     {
-        spawnMysteryBoxes();
+        //Simpleton
+        if (Instance != null)
+        {
+            Destroy(gameObject);
+        }
+        else
+            Instance = this;
+
+        playerCount = PhotonNetwork.CurrentRoom.PlayerCount;
+
+        ChangeGameState(GameState.Start);
+        
     }
 
     // Update is called once per frame
@@ -19,6 +46,32 @@ public class GameStartManager : MonoBehaviour
         
     }
 
+    void CheckGameState()
+    {
+        switch (gameState)
+        {
+
+            case GameState.Start:
+
+                spawnMysteryBoxes();
+                
+
+                break;
+
+            case GameState.End:
+
+
+                break;
+        }
+    }
+
+    public void ChangeGameState(GameState state)
+    {
+        gameState = state;
+        CheckGameState();
+    }
+
+    #region StartGame
     public void spawnMysteryBoxes()
     {
         for(int i =0; i<3; i++)
@@ -31,4 +84,12 @@ public class GameStartManager : MonoBehaviour
         }
        
     }
+
+
+    #endregion
+
+    #region EndGame
+
+
+    #endregion
 }
